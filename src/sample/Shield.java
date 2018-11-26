6b51b1ff5fc0c5ffc4e60acb2d3699ac144f19b0
Aayush Baghel <30633516+AayushBaghel@users.noreply.github.com>
2018-11-27 03:58:40 +0530

    Image Inserted

--- src/sample/Shield.java
+++ src/sample/Shield.java
@@ -1,9 +1,8 @@
 package sample;
 
 
-
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 import java.awt.geom.Point2D;
 
@@ -11,7 +10,14 @@ public class Shield {
     private Point2D velocity;
     private boolean alive = true;
 
-    private Rectangle body = new Rectangle(25, 30, Color.ORANGERED);
+    private StackPane body = new StackPane();
+
+    public Shield(){
+        ImageView img = new ImageView(Main.class.getResource("/Shield.png").toString());
+        img.setFitHeight(40);
+        img.setFitWidth(40);
+        body.getChildren().add(img);
+    }
 
     public boolean isAlive() {
         return alive;
@@ -25,7 +31,7 @@ public class Shield {
         this.velocity = velocity;
     }
 
-    public Rectangle getBody() {
+    public StackPane getBody() {
         return body;
     }
 }
